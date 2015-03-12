5396140716cb5f9d600de156a56142810878ae2e
Per-Gustaf Stenberg <per-gustaf.stenberg@stonepath.se>
2015-03-12 10:51:01 +0100

    Source code added

---
+++ src/Bridge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace BridgeLibrary
+{
+    public class Bridge
+    {
+        public delegate Packet BridgeDelegate(Packet command);
+
+        public static void receiveCommand(int port,BridgeDelegate bridgeDelegate)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, port);
+            tcpListener.Start();
+            TcpClient client = tcpListener.AcceptTcpClient();
+
+            NetworkStream clientStream = client.GetStream();
+
+            byte[] data = new byte[1024];
+
+            int receivedDataLength = clientStream.Read(data, 0, data.Length);
+            string commandData = Encoding.UTF8.GetString(data, 0, receivedDataLength);
+            
+            Packet command = JsonConvert.DeserializeObject<Packet>(commandData);
+            Packet returnCommand = bridgeDelegate(command);
+
+            string returnCommandData = JsonConvert.SerializeObject(returnCommand);
+
+            
+            clientStream.Write(Encoding.UTF8.GetBytes(returnCommandData), 0, returnCommandData.Length);
+
+            clientStream.Flush();
+
+            client.Close();
+            tcpListener.Stop();
+
+        }
+
+    }
+}
---
+++ src/Packet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BridgeLibrary
+{
+    public class Packet
+    {
+        private string _command;
+        private bool _containsError;
+        private string _errorMessage;
+        private string[] _arguments;
+
+        public string Command
+        {
+            get{return _command;}
+            set{_command = value;}
+        }
+        public bool ContainsError
+        {
+            get { return _containsError; }
+            set { _containsError = value; }
+        }
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; }
+        }
+        public string[] Arguments
+        {
+            get { return _arguments; }
+            set { _arguments = value; }
+        }
+
+    }
+}
